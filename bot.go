24f5066d02780c54ed87bbea4d068c8fc982a20e
IPauI <shiba.p@ya.ru>
2017-06-23 18:26:44 +0300

    Update bot.go
    Goroutines added

--- bot.go
+++ bot.go
@@ -64,23 +64,80 @@ func main() {
 	isUrlRequired := make(map[int]bool)      // checks whether url was requested by "/snap"
 	isRegisterRequired := make(map[int]bool) // checks whether name and pass number are needed to be passed
 	requests := make(map[int]uint32)         // map stores the status of user's request keyed by id
-	//TODO checking loop
-	for update := range updates {
-		if update.Message == nil {
-			continue
-		}
+	chatIDs := make(map[int]int64)
+	//Telegram Upds reuest loop
+	go func() {
+		for update := range updates {
+			if update.Message == nil {
+				continue
+			}
 
-		sender := func(m string) {
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, m)
-			msg.ReplyToMessageID = update.Message.MessageID
-			bot.Send(msg)
-		}
-		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+			sender := func(m string) {
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, m)
+				msg.ReplyToMessageID = update.Message.MessageID
+				bot.Send(msg)
+			}
+			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
 
-		// check results in this loop
+			// check results in this loop
+
+			/*checkSuggestion() encourages user to press "/check"
+			checkSuggestion := func() {
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "теперь тапай /check")
+				msg.ReplyToMessageID = update.Message.MessageID
+				bot.Send(msg)
+			}*/
+			curUserId := update.Message.From.ID
+			switch {
+			case isUrlRequired[curUserId]:
+				isUrlRequired[curUserId] = false
+				switch {
+				case strings.HasPrefix(update.Message.Text, "http://"):
+					curUrl[curUserId] = update.Message.Text
+				default:
+					curUrl[curUserId] = "http://" + update.Message.Text
+				}
+				requests[curUserId] = snapper(curUrl[curUserId])
+				chatIDs[curUserId] = update.Message.Chat.ID
+				sender("твой запрос принят, жди обновлений")
+				//sender(fmt.Sprint(oldSnap[curUserId]))
+				//checkSuggestion()
+
+			case strings.HasPrefix(update.Message.Text, "/snap"):
+				sender("введи ссылку (желательно скопировать прямо из броузера)")
+				isUrlRequired[curUserId] = true
+			case isRegisterRequired[curUserId]:
+				words := strings.Split(update.Message.Text, " ")
+				v := url.Values{}
+				v.Set("number", words[1])
+				v.Set("surname", words[0])
+				curUrl[curUserId] = "http://orcoko.ru/ege/results-ege/?" + v.Encode()
+				isRegisterRequired[curUserId] = false // register completed
+				snap := snapper(curUrl[curUserId])
+				//check whether snap was taken successfully
+				if snap == 0 {
+					sender("перепроверь свои данные и попробуй еще /register")
+					continue
+				}
+				requests[curUserId] = snap
+				chatIDs[curUserId] = update.Message.Chat.ID
+				sender("Твой запрос принят, жди обновлений")
+				//checkSuggestion()
+
+			case update.Message.Text == "/register":
+				sender("введите фамилию и номер паспорта через пробел")
+				isRegisterRequired[curUserId] = true
+			}
+		}
+	}()
+	//Request checks loop
+	go func() {
 		for id, snap := range requests {
 			if snap != snapper(curUrl[id]) {
-				sender("Дождались! Смотри здесь: " + curUrl[id])
+				//sender("Дождались! Смотри здесь: " + curUrl[id])
+				msg := tgbotapi.NewMessage(chatIDs[id], "Дождались! Смотри здесь: "+curUrl[id])
+				//msg.ReplyToMessageID = update.Message.MessageID
+				bot.Send(msg)
 				/*maps : = []*map[int]interface{}{&curUrl, &isUrlRequired, &isRegisterRequired, &requests}
 				for _, pointer := range maps{
 					delete(*pointer, id)
@@ -91,61 +148,5 @@ func main() {
 				delete(requests, id)
 			}
 		}
-
-		/*checkSuggestion() encourages user to press "/check"
-		checkSuggestion := func() {
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "теперь тапай /check")
-			msg.ReplyToMessageID = update.Message.MessageID
-			bot.Send(msg)
-		}*/
-		curUserId := update.Message.From.ID
-		switch {
-		case isUrlRequired[curUserId]:
-			isUrlRequired[curUserId] = false
-			switch {
-			case strings.HasPrefix(update.Message.Text, "http://"):
-				curUrl[curUserId] = update.Message.Text
-			default:
-				curUrl[curUserId] = "http://" + update.Message.Text
-			}
-			requests[curUserId] = snapper(curUrl[curUserId])
-			sender("твой запрос принят, жди обновлений")
-			//sender(fmt.Sprint(oldSnap[curUserId]))
-			//checkSuggestion()
-
-		case strings.HasPrefix(update.Message.Text, "/snap"):
-			sender("введи ссылку (желательно скопировать прямо из броузера)")
-			isUrlRequired[curUserId] = true
-		case isRegisterRequired[curUserId]:
-			words := strings.Split(update.Message.Text, " ")
-			v := url.Values{}
-			v.Set("number", words[1])
-			v.Set("surname", words[0])
-			curUrl[curUserId] = "http://orcoko.ru/ege/results-ege/?" + v.Encode()
-			isRegisterRequired[curUserId] = false // register completed
-			snap := snapper(curUrl[curUserId])
-			//check whether snap was taken successfully
-			if snap == 0 {
-				sender("перепроверь свои данные и попробуй еще /register")
-				continue
-			}
-			requests[curUserId] = snap
-			sender("Твой запрос принят, жди обновлений")
-			//checkSuggestion()
-
-		case update.Message.Text == "/register":
-			sender("введите фамилию и номер паспорта через пробел")
-			isRegisterRequired[curUserId] = true
-
-			/*this case used to be important
-			case update.Message.Text == "/check":
-				sender("начинаю чекать. я напишу тебе, если что-то случится. но помни, без перерыва я могу работать только 6 часов!")
-				newSnap := snapper(curUrl[curUserId])
-				startTime := time.Now()
-				for (newSnap == oldSnap[curUserId]) && (time.Since(startTime) < time.Duration(6*time.Hour)) {
-					newSnap = snapper(curUrl[curUserId])
-				}
-				sender("кажется, что-то изменилось проверь на " + curUrl[curUserId])*/
-		}
-	}
+	}()
 }
